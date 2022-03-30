65b42a5d3a10e5b2f9ad8f572daf1affba997d9f
Ndlovu Riot <2096330@students.wits.ac.za>
2022-03-30 23:19:48 +0200

    firebase integration

--- project/App.js
+++ project/App.js
@@ -1,6 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZVQZO8XWR0UTPoSMLrRN7nSltUDW9Xzs",
+  authDomain: "dfmpc-student-placement-system.firebaseapp.com",
+  projectId: "dfmpc-student-placement-system",
+  storageBucket: "dfmpc-student-placement-system.appspot.com",
+  messagingSenderId: "295214875936",
+  appId: "1:295214875936:web:c6574b5bd48a1652c84345",
+  measurementId: "G-C2X38C34FX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export default function App() {
   return (
     <View style={styles.container}>
