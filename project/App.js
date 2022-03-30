e94d31c42d4cc900d5ca218784591bfc3f660cc7
Mtho-hub <mtk2masa@gmail.com>
2022-03-30 15:52:00 +0200

    Update App.js

--- project/App.js
+++ project/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Mthoko</Text>
+      <Text>Students</Text>
       <StatusBar style="auto" />
     </View>
   );
