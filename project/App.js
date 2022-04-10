54447f8b9663c83483b528b4c3e52912ac27ea5f
Nokuthaba <710178@students.wits.ac.za>
2022-04-10 11:44:27 +0200

    Update App.js

--- project/App.js
+++ project/App.js
@@ -1,39 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@nokuthabam 
-Software-Design-2022
-/
-DFMPC-Student-Placement-System
-Private
-Code
-Issues
-1
-Pull requests
-1
-Actions
-Projects
-1
-Security
-2
-Insights
-Settings
-We found potential security vulnerabilities in your dependencies.
-You can see this message because you have been granted access to Dependabot alerts for this repository.
-
-DFMPC-Student-Placement-System/project/App.js /
-@peacendlovu
-peacendlovu Update App.js
-Latest commit fc8a677 yesterday
- History
- 6 contributors
-@peacendlovu@CallumMuller@shgnplaatjies@Mtho-hub@Cipher73@nokuthabam
-33 lines (28 sloc)  933 Bytes
-   
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
@@ -67,16 +31,3 @@ const styles = StyleSheet.create({
   },
 });
 export default App;
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading complete
