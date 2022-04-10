b559c726e0cc7708c08958f41a1a171647f57a62
Cipher73 <2345106@students.wits.ac.za>
2022-04-11 00:52:29 +0200

    New login implementation with realtime database instead of Auth

--- project/Screens/Login.js
+++ project/Screens/Login.js
@@ -1,34 +1,67 @@
 import React, { useState ,useEffect} from 'react'
-import {Platform,Keyboard, TouchableWithoutFeedback,TouchableOpacity,KeyboardAvoidingView,StyleSheet, Text, TextInput, View } from 'react-native'
-import {auth} from "../firebase"
+import {Platform,Keyboard, TouchableWithoutFeedback,TouchableOpacity,KeyboardAvoidingView,StyleSheet, Text,Button, TextInput, View,Alert } from 'react-native'
+import {firebase} from "../firebase"
 import {useNavigation} from '@react-navigation/core'
 
 
 const Login = () => {
 
+
     const [email,setEmail]= useState('')
     const [password,setPassword]= useState('')
     const navigation =useNavigation()
+
+
+
+    function showAlert(title, message) {
+  Alert.alert(
+    title,
+    message,
+    [
+      {
+        text: "Cancel",
+       // onPress: () => Alert.alert("Cancel Pressed"),
+        style: "cancel",
+      },
+    ],
+    {
+      cancelable: true,
+     
+    }
+  );
+    }
     const LoginFirebase =() =>{
         // login with email and password
-        auth.signInWithEmailAndPassword(email,password).then(userCredentials =>{
-            const user =userCredentials.user;
 
-            auth.onAuthStateChanged(user =>{
-                // if user has successfully logged in then the dashboard appears
-                if(user){navigation.navigate("Dashboard")}
-          
-            })
-        }).catch(error => alert(error.message));
+        // check if email exists in the database if so then hash pass and compare 
 
+        var found = false
+        firebase.database().ref('/users').on('value',snapshot=>{
+            //if(snapshot.val()==email)
+               const key = snapshot.forEach(function(data) {
+     
+                const check_email =snapshot.child(data.key+"/email").val();
+             
+                const encrypted = snapshot.child(data.key+"/password_digest").val();
 
+               if(check_email===email){
+                   found =true
+              
+              
+                    if(encrypted===password){
+                        navigation.navigate("Dashboard")
+                    }
+                    else{
+                        showAlert("Password Error","Your email and password do not match")
+                    }
+                }              
+            });
+           if(found===false){
+            showAlert("Email Error","user does not exist")
+           }
+        })
       }
-      
-
-
       // this allows you to switch between different screens
-
-
 
   return (
 
@@ -61,18 +94,19 @@ const Login = () => {
         <TouchableOpacity
             onPress={LoginFirebase}   // when user clicks on login button 
             >
-
             <Text style={styles.buttonText}>Login</Text>
 
         </TouchableOpacity>
     </View>
-    
+  
+
     </KeyboardAvoidingView>
    // </TouchableWithoutFeedback>
   )
 }
 
 export default Login
+
 
 const styles = StyleSheet.create({
     container: {
@@ -106,5 +140,14 @@ color:"blue",
 fontSize:16,
 fontWeight:"bold",
 },       
-
+btn:{
+    marginTop:50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'green',
+}
 })
