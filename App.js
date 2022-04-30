07183e5486a5c22c0c9e1ad5a2f094d16cd6f56c
shgnplaatjies <plaatjiesshagan@gmail.com>
2022-04-30 14:19:19 +0200

    Added settings button to header
    Added settings button to header
    onPress function to be implemented

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Login from './Screens/Login'
-import Dashboard from './Screens/Dashboard'
-import Schedule from './Screens/Schedule'
-import Calendar from './Screens/Calendar'
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./Screens/Login";
+import Dashboard from "./Screens/Dashboard";
+import Schedule from "./Screens/Schedule";
+import Calendar from "./Screens/Calendar";
 
 const Stack = createNativeStackNavigator();
-
-
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{headerShown:false}} name="Login" component={Login} />
-        <Stack.Screen name="Dashboard" component={Dashboard} />
+        {/* <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={Login}
+        /> */}
+        <Stack.Screen
+          name="Dashboard"
+          component={Dashboard}
+          options={{
+            headerTitle: "Dashboard",
+            headerRight: () => (
+              <Button title="Settings" onPress={alert("Go to settings")} />
+            ),
+          }}
+        />
         <Stack.Screen name="Schedule" component={Schedule} />
         <Stack.Screen name="Calendar" component={Calendar} />
       </Stack.Navigator>
@@ -27,9 +38,9 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#00ffff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 export default App;
