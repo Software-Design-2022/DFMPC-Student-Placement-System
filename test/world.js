08af33e1d93b39acfb03a91739983275c7634031
ndlovu riot <peace.r.ndlovu@gmail.com>
2022-05-11 02:17:05 +0200

    testing, github workflows config

---
+++ test/world.js
@@ -0,0 +1,9 @@
+function world(num) {
+    if (num % 2 == 0) {
+      return "Hello Even World"
+    } else {
+      return "Hello Odd World"
+    }
+}
+
+module.exports = world;
---
+++ test/world.test.js
@@ -0,0 +1,9 @@
+const world = require('./world');
+
+test('tests 2 is even', () => {
+    expect(world(2)).toBe("Hello Even World");
+});
+
+test('tests 3 is odd', () => {
+    expect(world(3)).toBe("Hello Odd World");
+});
