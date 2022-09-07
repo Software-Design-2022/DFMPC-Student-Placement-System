032530775d63e62570a1f5d68151ad4b323afa93
Cipher73 <2345106@students.wits.ac.za>
2022-09-07 12:49:40 +0200

    merge with peace branch

--- app.config.js
+++ app.config.js
@@ -7,11 +7,7 @@ export default {
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
-<<<<<<< HEAD
     "plugins": ["@react-native-voice/voice","@notifee/react-native"],
-=======
-    "plugins": [],
->>>>>>> origin/peace
     "splash": {
       "image": "./assets/splash.png",
       "resizeMode": "contain",
